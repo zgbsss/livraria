6e7a8955dfbb5427a897120ea494072fb057218e
zgbsss <gabrielasaraiva.04@gmail.com>
2023-03-07 16:47:29 -0300

    criação da model Autor

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from livraria.models import Categoria, Editora
+from livraria.models import Autor, Categoria, Editora 
 
+
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 
--- livraria/models.py
+++ livraria/models.py
@@ -13,4 +13,12 @@ class Editora(models.Model):
     def __str__(self):
         return self.nome
 
-# Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.email}"
+    
+    class Meta:
+        verbose_name_plural = "Autores"
