cce1d1909174ae377d29c1dd2c5ff582850a6dd6
zgbsss <gabrielasaraiva.04@gmail.com>
2023-07-04 16:35:36 -0300

    Modificando o Usuário Padrão

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
+    "usuario",
     "livraria",
 ]
 
@@ -137,3 +138,5 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+
+AUTH_USER_MODEL = "usuario.Usuario"
--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,8 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from usuario.router import router as usuario_router
+
 router = DefaultRouter()
 router.register(r"autores", AutorViewSet)
 router.register(r"categorias", CategoriaViewSet)
@@ -35,4 +37,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include(usuario_router.urls)),
 ]
