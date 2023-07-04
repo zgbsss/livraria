a39e44f63003619d1be0322eb688649bd442c5b6
zgbsss <gabrielasaraiva.04@gmail.com>
2023-07-04 16:45:09 -0300

    Separação do Arquivo Serializers

---
+++ livraria/serializers/livro.py
@@ -0,0 +1,19 @@
+from rest_framework.serializers import ModelSerializer
+
+from livraria.models import Livro
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
+
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ["id", "titulo", "preco"]
